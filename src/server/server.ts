159b9e3f3f958f86c1d96c9ac956a64be7e738dc
Harmandeep Singh <deepharman2000@gmail.com>
2021-02-18 21:34:41 -0500

    added Procfile for Heroku

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,15 +1,16 @@
 import * as express from "express"; //stick with this syntex, as specified in the cofig files
-
+import * as cors from "cors";
 import Routers from './routers';
 
 let app = express();
-
+app.use(cors())
 app.use(express.static('public'));
 app.use(Routers);
 app.use(express.json())
 
 
+const port = process.env.PORT || 3000
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`listening at port 3000`);
 })
