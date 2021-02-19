fcc0d4817af48ed9fd36e6a4e1c7ef3f64ad3d08
Harmandeep Singh <deepharman2000@gmail.com>
2021-02-18 22:05:50 -0500

    added build script

--- src/server/server.ts
+++ src/server/server.ts
@@ -12,5 +12,5 @@ app.use(express.json())
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-    console.log(`listening at port 3000`);
+    console.log(`listening at port ${port}`);
 })
