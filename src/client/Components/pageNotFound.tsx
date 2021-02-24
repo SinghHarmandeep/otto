d224a75e1f33521f15ee1db61812473fa9672e7c
Harmandeep Singh <deepharman2000@gmail.com>
2021-02-24 18:23:11 -0500

    minor bug fix related to page not found

--- src/client/Components/pageNotFound.tsx
+++ src/client/Components/pageNotFound.tsx
@@ -9,7 +9,7 @@ const error = () => {
                     <title>404 page not found</title>
                 </Helmet>
                 <h1 className="d-flex justify-content-center text-warning">404 page not found</h1>
-                <Link className='d-flex justify-content-center text-light' to='/findRide'>Return Home</Link>
+                <Link className='d-flex justify-content-center' to='/'>Return Home</Link>
             </div>
     )
 }
