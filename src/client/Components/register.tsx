369422da7d7d24051816e32c957457cd945111bd
Harmandeep Singh <deepharman2000@gmail.com>
2021-03-26 14:16:24 -0400

    added register page

--- src/client/Components/register.tsx
+++ src/client/Components/register.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { StringLiteralLike } from 'typescript';
 
-class Login extends React.Component<Props, State> {
+class Register extends React.Component<Props, State> {
     constructor(props: any) {
         super(props)
     }
     render() {
         return (
             <div className= 'container'>
-                <h1>LogIn page</h1>
-                <form action="../login" method='POST'>
+                <h1>Register</h1>
+                <form action="../register" method='POST'>
                     <input type="text" name="username" id="" placeholder='Cockroach' />
+                    <input type="email" name="email" id="" placeholder='takeit@drop.off' />
+                    <input type="number" name="phone" id="" placeholder='123456789' />
                     <input type="password" name='pass' placeholder='********' />
                     <button type="submit" className="btn btn-primary col-12">Submit</button>
                 </form>
@@ -24,7 +27,9 @@ interface Props {
   
   interface State {
       username: String,
-      password: String
+      password: String,
+      email: String,
+      number: number
   }
 
-export default Login
+export default Register
