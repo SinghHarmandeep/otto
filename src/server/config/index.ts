e3cfaeb1fbcb75a8dc842c31b12d7f7fc70243bd
Harmandeep Singh <deepharman2000@gmail.com>
2021-03-29 22:15:41 -0400

    refactored config folder, removed dev and prod.ts files and connected the index to dotenv file, straight

--- src/server/config/index.ts
+++ src/server/config/index.ts
@@ -2,4 +2,14 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-export default require(`./${process.env.NODE_ENV}`).default;
+// export default require(`./${process.env.NODE_ENV}`).default;
+//add production, development.ts and add appropriate secure variables 
+//or do the following
+
+export default {
+    GMapAPI: process.env.G_MAP_API,
+    MongoURI: process.env.MONGOURI,
+    auth: {
+        secret: process.env.SECRET
+    }
+}
--- src/server/config/production.ts
+++
@@ -1,8 +0,0 @@
-
-export default {
-    // GMapAPI: process.env.G_MAP_API,
-    MongoURI: process.env.MONGOURI,
-    auth: {
-        secret: process.env.SECRET
-    }
-}
