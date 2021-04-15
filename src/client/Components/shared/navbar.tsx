3490854d5fc95d5c25270c4c759b6bd5bfa33fce
Harmandeep Singh <deepharman2000@gmail.com>
2021-04-14 20:07:37 -0400

    working on the navbar making it dynamic based on the userlogin status jwt-token

--- src/client/Components/shared/navbar.tsx
+++ src/client/Components/shared/navbar.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav'
 
+import { AccessToken } from '../../utils/app'
+import jwtDecode from "jwt-decode";
+
 const navbar = () => {
+
+    const [user, setUser] = useState(AccessToken);
+
+    if (user) {
+        console.log(user);
+        let decodedUser = jwtDecode(user)
+    }
     return (
         <Navbar bg="dark" variant="dark">
             <Navbar.Brand href="/">Otto</Navbar.Brand>
@@ -15,11 +25,19 @@ const navbar = () => {
                 <Nav.Link href="pricing">Pricing</Nav.Link>
                 <Nav.Link href="contact">Contact Us</Nav.Link>
             </Nav>
+
+            {user ? (
+                <div>
+                    <Nav.Link className="my-2 my-sm-0 text-success" href="login">{decodedUser?.handle}</Nav.Link>
+                </div>
+            ) : (
             <form className="form-inline my-2 my-lg-0">
-                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
                 <Nav.Link className="my-2 my-sm-0 text-success" href="login">Sign in</Nav.Link>
                 <Nav.Link className="btn btn-outline-success my-2 my-sm-0" href="signup">Get started</Nav.Link>
             </form>
+            )
+            }
         </Navbar>
     )
 }
