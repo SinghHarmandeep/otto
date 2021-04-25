8dd96c758299e30c62a78fcaf538913a0f3dc267
Harmandeep Singh <deepharman2000@gmail.com>
2021-04-25 17:54:34 -0400

    cleaned the register page ui fields

--- src/client/Components/auth/register.tsx
+++ src/client/Components/auth/register.tsx
@@ -10,21 +10,38 @@ class Register extends React.Component<Props, State> {
             <div className='container'>
                 <h1>Register</h1>
                 <form action="../signup" method='POST'>
-                    <label htmlFor="username">Username</label>
-                    <input type="text" name="username" placeholder='Almighty' />
-                    <br /><br />
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" name="email" placeholder='takeit@drop.off' />
-                    <br /><br />
-                    <label htmlFor="pass">Password</label>
-                    <input type="password" name='pass' placeholder='********' />
-                    <br /><br />
-                    <label htmlFor="pass">Confirm Password</label>
-                    <input type="password" name='re-pass' placeholder='********' />
-                    <br /><br />
-                    <label htmlFor="phone">Phone no:</label>
-                    <input type="number" name="phone" placeholder='123456789' />
-                    <br /><br />
+
+                    <div className="form-group row">
+                        <label htmlFor="username" className="col-sm-2 col-form-label">Username</label>
+                        <div className='col-sm-10'>
+                            <input type="text" name="username" placeholder='Almighty' className='form-control' />
+                        </div>
+
+                    </div>
+                    <div className="form-group row">
+                        <label htmlFor="email" className="col-sm-2 col-form-label">Email:</label>
+                        <div className='col-sm-10'>
+                            <input type="email" name="email" placeholder='takeit@drop.off' className='form-control' />
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label htmlFor="pass" className="col-sm-2 col-form-label">Password</label>
+                        <div className='col-sm-10'>
+                            <input type="password" name='pass' placeholder='********' className='form-control' />
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label htmlFor="pass" className="col-sm-2 col-form-label">Confirm Password</label>
+                        <div className='col-sm-10'>
+                            <input type="password" name='re-pass' placeholder='********' className='form-control' />
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label htmlFor="phone" className="col-sm-2 col-form-label">Phone no:</label>
+                        <div className='col-sm-10'>
+                            <input type="number" name="phone" placeholder='123456789' className='form-control' />
+                        </div>
+                    </div>
                     <button type="submit" className="btn btn-primary col-12">Sign Up</button>
                     <br /><br />
                     <Link to="/login" className="text-dark">Have an account? <b> Sing In </b></Link>
