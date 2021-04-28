cd7811b43e9f5d8ebaa7c7597dd238f7f550e661
Harmandeep Singh <deepharman2000@gmail.com>
2021-04-27 22:38:00 -0400

    changed the theam

--- src/client/Components/shared/home.tsx
+++ src/client/Components/shared/home.tsx
@@ -11,9 +11,9 @@ const home = () => {
             </h2>
             <h2 className='text-center'>We make it easy like 1 2 3.</h2>
             <ul className="list-group text-center container">
-                <li className="list-group-item">1. Choose pickup location</li>
-                <li className="list-group-item ">2. Provide package details</li>
-                <li className="list-group-item">3. Choose dropoff location</li>
+                <li className="list-group-item text-dark">1. Choose pickup location</li>
+                <li className="list-group-item text-dark">2. Provide package details</li>
+                <li className="list-group-item text-dark">3. Choose dropoff location</li>
             </ul>
             <h1 >
                 Horrary that's all. Now you pay us for the exclusive service and we
