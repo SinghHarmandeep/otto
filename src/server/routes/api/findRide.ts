2849a9983ac77f87a41797da44e7facd018fb6c6
Harmandeep Singh <deepharman2000@gmail.com>
2021-05-09 16:51:48 -0400

    removed unnecessary comments

--- src/server/routes/api/findRide.ts
+++ src/server/routes/api/findRide.ts
@@ -1,19 +1,19 @@
 import * as express from "express";
-import * as passport from 'passport';
+import { authenticate } from 'passport';
 
 const router = express.Router();
 
 
 router.post('/findride',
-    passport.authenticate('jwt'),
+    authenticate('jwt'),
     (req, res) => {
         //data from request
         let order = req.body;
-        
+
         //user info after verifing
         let user = req.user
-        
-        res.status(201).json({order, user});
+
+        res.status(201).json({ order, user });
 
     })
 
