36749edd27ef4964e6b6e2ad74a83b800abad644
Harmandeep Singh <deepharman2000@gmail.com>
2021-05-20 22:30:28 -0400

    updating mapView page

--- src/client/Components/shared/form.tsx
+++ src/client/Components/shared/form.tsx
@@ -9,54 +9,42 @@ const form = () => {
     const [origin, setOrigin] = useState('');
     const [dest, setDest] = useState('');
     return (
-        <div className='container'>
-            <div className='row'>
-                <form onSubmit={e => handleSubmit(e)} className='col'>
-                    <div className="form-group container p-3 my-3 ">
+        <div className='' style={{ color: 'black' }}>
+            <div>
+
+                <form onSubmit={e => handleSubmit(e)} className=''>
+                    <div className="form-group col">
                         <label>Pick up address:</label>
-                        <input type="text" className="form-control" value={origin} name="pickup"
+                        <input type="text" className="form-control col-4" value={origin} name="pickup"
                             onChange={e => { setOrigin(e.target.value) }} placeholder="123 pickup street" />
                         <small id="emailHelp" className="form-text text-muted">We'll never share your pickup address with anyone else.</small>
                     </div>
-                    <div className="form-group container p-3 my-3 ">
+                    <div className="form-group col p-3 my-3 ">
                         <label >Item Discription</label>
-                        <input type="text" className="form-control" name="item" placeholder="Book, Keys, Food, package etc" />
+                        <input type="text" className="form-control col-4" name="item" placeholder="Book, Keys, Food, package etc" />
                     </div>
-                    <div className="form-group container p-3 my-3 ">
+                    <div className="form-group col p-3 my-3 ">
                         <label>Drop off address:</label>
-                        <input type="text" className="form-control" value={dest} name="dropoff" onChange={e => { setDest(e.target.value) }} placeholder="123 drop street" />
+                        <input type="text" className="form-control col-4" value={dest} name="dropoff" onChange={e => { setDest(e.target.value) }} placeholder="123 drop street" />
                         <small id="emailHelp" className="form-text text-muted">We'll never share your drop off with anyone else.</small>
                     </div>
-                    <div className='container p-3 my-3 '>
-                        <div className="form-group row">
-                            <label className="col-2 col-form-label">Date</label>
-                            <div className="col-10">
-                                <input className="form-control" type="date" name="pickupdate" />
-                            </div>
+                    <div className='form-group col p-3 my-3 '>
+                        <div className="col form-group row">
+                            <label>Date:</label>
+                            <input className="form-control col-2" type="date" name="pickupdate" />
                         </div>
-                        <div className="form-group row">
-                            <label className="col-2 col-form-label">Time</label>
-                            <div className="col-10">
-                                <input className="form-control" type="time" name="pickuptime" />
-                            </div>
+                        <div>
+                            <label >Time: </label>
+                            <input className="form-control col-2" type="time" name="pickuptime" />
                         </div>
                     </div>
-                    <button type="submit" className="btn btn-primary col-12">Let's go!</button>
+                    <div className='form-group col p-3 my-3'>
+                        <button type="submit" className="btn btn-primary col-4">Let's go!</button>
+                    </div>
                 </form>
 
                 <div
-                    style={{
-                        // position: 'absolute' as 'absolute',
-                        position: 'fixed',
-                        left: '0px',
-                        top: '0px',
-                        width: '100%',
-                        height: '100%',
-                        zIndex: -1,
-                        backgroundColor: '#212529',
-                        // backgroundColor: 'blue',
-                        overflowY: 'hidden'
-                    }}>
+                    style={mapStyle}>
                     <Map orig={(origin) ? origin : 'new york'} dest={(dest) ? dest : 'new york'} />
                 </div>
             </div>
@@ -75,11 +63,23 @@ function handleSubmit(e: FormEvent<HTMLFormElement>) {
         console.log(err);
         showAlert('you must be logedin to make this request!')
     });
-
 }
 
 function showAlert(msg: string) {
     alert(msg)
+}
+
+const mapStyle = {
+    // position: 'absolute' as 'absolute',
+    position: 'fixed' as 'fixed',
+    left: '0px',
+    top: '0px',
+    width: '100%',
+    height: '100%',
+    zIndex: -1,
+    backgroundColor: '#212529',
+    // backgroundColor: 'blue',
+    overflowY: 'hidden' as 'hidden'
 }
 
 export default form;
