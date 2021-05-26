c65573d4cd220b2f146eafbeb5c7b36ffe39a619
Harmandeep Singh <deepharman2000@gmail.com>
2021-05-25 21:21:40 -0400

    modularized the form connected the fields to parent component

---
+++ src/client/Components/shared/details.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+class Details extends React.Component<IDetailsProps> {
+    constructor(props: any) {
+        super(props)
+        this.state = null;
+    }
+
+
+    render() {
+        // console.log(this.props.orgn + "this is it");
+        // console.log(this.props.dest);
+
+        // return (
+        //     <h1>{this.props.dest}</h1>
+        // );
+        return (
+            <form
+                onSubmit={e => this.props.submit(e)} 
+                className='my-2'>
+
+                <label className="mt-2">Pick up address:</label>
+                <input type="text" className="form-control"
+                    // value={this.props.orgn}
+                    name="pickup"
+                    onChange={e => { this.props.pickUp(e.target.value) }} placeholder="123 pickup street" />
+
+                <label className="mt-2">Item Discription:</label>
+                <input type="text" className="form-control" name="item" placeholder="Book, Keys, Food, package etc" />
+
+
+                <label className="mt-2">Drop off address:</label>
+                <input type="text" className="form-control"
+                    // value={this.props.dest}
+                    name="dropoff"
+                    onChange={e => { this.props.dropOff(e.target.value) }} placeholder="123 drop street" />
+
+                <div className="row mt-2">
+                    <div className='col'>
+                        <label>Date:</label>
+                        <input className="form-control col" type="date" name="pickupdate" />
+                    </div>
+
+                    <div className='col'>
+                        <label>Time:</label>
+                        <input className="form-control col" type="time" name="pickuptime" />
+                    </div>
+                </div>
+                <button type="submit" className="btn btn-primary col my-2">Let's go!</button>
+            </form>)
+    }
+}
+
+interface IDetailsProps {
+    dest: string,
+    orgn: string,
+    pickUp: any,
+    dropOff: any,
+    submit: any
+}
+
+export default Details;
--- src/client/Components/shared/form.tsx
+++ src/client/Components/shared/form.tsx
@@ -3,62 +3,84 @@ import React, { FormEvent, useState } from 'react';
 import { request } from "../../utils/app";
 
 import Map from './mapView';
+import Detials from './details'
 
-const form = () => {
+// const form = (props: any) => {
 
-    const [origin, setOrigin] = useState('');
-    const [dest, setDest] = useState('');
-    return (
-        <div className='col-4 bg-dark mx-2 rounded shadow-lg'>
-            <form onSubmit={e => handleSubmit(e)} className='my-2'>
+class Form extends React.Component<IFormProps, IFormState> {
+    // const [origin, setOrigin] = useState('');
+    // const [dest, setDest] = useState('');
 
-                <label className="mt-2">Pick up address:</label>
-                <input type="text" className="form-control" value={origin} name="pickup"
-                    onChange={e => { setOrigin(e.target.value) }} placeholder="123 pickup street" />
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            orgn: '',
+            dest: ''
+        }
+    }
+    render() {
+        console.log(this.state);
 
-                <label className="mt-2">Item Discription:</label>
-                <input type="text" className="form-control" name="item" placeholder="Book, Keys, Food, package etc" />
+        return (
+            <div className='col-4 bg-dark mx-2 rounded shadow-lg' >
+
+                {/* <form onSubmit={e => this.handleSubmit(e)} className='my-2'>
+                        <label className="mt-2">Pick up address:</label>
+                        <input type="text" className="form-control" value={this.state.orgn} name="pickup"
+                            onChange={e => { this.setState({ orgn: e.target.value }) }} placeholder="123 pickup street" />
+
+                        <label className="mt-2">Item Discription:</label>
+                        <input type="text" className="form-control" name="item" placeholder="Book, Keys, Food, package etc" />
 
 
-                <label className="mt-2">Drop off address:</label>
-                <input type="text" className="form-control" value={dest} name="dropoff" onChange={e => { setDest(e.target.value) }} placeholder="123 drop street" />
+                        <label className="mt-2">Drop off address:</label>
+                        <input type="text" className="form-control" value={this.state.dest} name="dropoff" 
+                        onChange={e => { this.setState({ dest: e.target.value }) }} placeholder="123 drop street" />
 
-                <div className="row mt-2">
-                    <div className='col'>
-                        <label>Date:</label>
-                        <input className="form-control col" type="date" name="pickupdate" />
-                    </div>
+                        <div className="row mt-2">
+                            <div className='col'>
+                                <label>Date:</label>
+                                <input className="form-control col" type="date" name="pickupdate" />
+                            </div>
 
-                    <div className='col'>
-                        <label>Time:</label>
-                        <input className="form-control col" type="time" name="pickuptime" />
-                    </div>
+                            <div className='col'>
+                                <label>Time:</label>
+                                <input className="form-control col" type="time" name="pickuptime" />
+                            </div>
+                        </div>
+                        <button type="submit" className="btn btn-primary col my-2">Let's go!</button>
+                    </form> */}
+
+                {/* < Detials dest={this.state.dest} orgn={this.state.orgn} pickUp={this.pickUp} dropOff={this.dropOff} /> */}
+                <div style={mapStyle}>
+                    <Map orig={(this.state.orgn) ? this.state.orgn : 'new york'} dest={(this.state.dest) ? this.state.dest : 'new york'} />
                 </div>
-                <button type="submit" className="btn btn-primary col my-2">Let's go!</button>
-            </form>
 
-            <div style={mapStyle}>
-                <Map orig={(origin) ? origin : 'new york'} dest={(dest) ? dest : 'new york'} />
             </div>
+        )
+    }
+    
+    
+    pickUp = (val: string) => this.setState({orgn: val})
+    dropOff = (val: string) => this.setState({dest: val})
 
-        </div>
-    )
-}
+    handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        
+        e.preventDefault()
+        // setAuth(localStorage.getItem('token'))
+        let res = request('/findride', 'POST', { data: 'data sent is the following' })
+        res.then(res => {
+            console.log(res.data);
+        }).catch(err => {
+            console.log(err);
+            this.showAlert('you must be logedin to make this request!')
+        });
+    }
 
-function handleSubmit(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault()
-    // setAuth(localStorage.getItem('token'))
-    let res = request('/findride', 'POST', { data: 'data sent is the following' })
-    res.then(res => {
-        console.log(res.data);
-    }).catch(err => {
-        console.log(err);
-        showAlert('you must be logedin to make this request!')
-    });
-}
+    showAlert = (msg: string) => {
+        alert(msg)
+    }
 
-function showAlert(msg: string) {
-    alert(msg)
 }
 
 const mapStyle = {
@@ -74,4 +96,14 @@ const mapStyle = {
     overflowY: 'hidden' as 'hidden'
 }
 
-export default form;
+
+
+interface IFormState {
+    orgn: string,
+    dest: string
+}
+interface IFormProps {
+    ar: any
+}
+
+export default Form;
