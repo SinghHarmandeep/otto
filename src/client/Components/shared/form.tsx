30903e9aee17096a669483437a9ba932ee48565a
Harmandeep Singh <deepharman2000@gmail.com>
2021-05-25 21:27:18 -0400

    succesful modul detailsform file and props working

--- src/client/Components/shared/form.tsx
+++ src/client/Components/shared/form.tsx
@@ -24,48 +24,30 @@ class Form extends React.Component<IFormProps, IFormState> {
         return (
             <div className='col-4 bg-dark mx-2 rounded shadow-lg' >
 
-                {/* <form onSubmit={e => this.handleSubmit(e)} className='my-2'>
-                        <label className="mt-2">Pick up address:</label>
-                        <input type="text" className="form-control" value={this.state.orgn} name="pickup"
-                            onChange={e => { this.setState({ orgn: e.target.value }) }} placeholder="123 pickup street" />
-
-                        <label className="mt-2">Item Discription:</label>
-                        <input type="text" className="form-control" name="item" placeholder="Book, Keys, Food, package etc" />
-
-
-                        <label className="mt-2">Drop off address:</label>
-                        <input type="text" className="form-control" value={this.state.dest} name="dropoff" 
-                        onChange={e => { this.setState({ dest: e.target.value }) }} placeholder="123 drop street" />
-
-                        <div className="row mt-2">
-                            <div className='col'>
-                                <label>Date:</label>
-                                <input className="form-control col" type="date" name="pickupdate" />
-                            </div>
-
-                            <div className='col'>
-                                <label>Time:</label>
-                                <input className="form-control col" type="time" name="pickuptime" />
-                            </div>
-                        </div>
-                        <button type="submit" className="btn btn-primary col my-2">Let's go!</button>
-                    </form> */}
-
-                {/* < Detials dest={this.state.dest} orgn={this.state.orgn} pickUp={this.pickUp} dropOff={this.dropOff} /> */}
+                < Detials
+                    dest={this.state.dest}
+                    orgn={this.state.orgn}
+                    pickUp={this.pickUp}
+                    dropOff={this.dropOff}
+                    submit={this.handleSubmit}
+                />
                 <div style={mapStyle}>
-                    <Map orig={(this.state.orgn) ? this.state.orgn : 'new york'} dest={(this.state.dest) ? this.state.dest : 'new york'} />
+                    <Map
+                        orig={(this.state.orgn) ? this.state.orgn : 'new york'}
+                        dest={(this.state.dest) ? this.state.dest : 'new york'}
+                    />
                 </div>
 
             </div>
         )
     }
-    
-    
-    pickUp = (val: string) => this.setState({orgn: val})
-    dropOff = (val: string) => this.setState({dest: val})
+
+
+    pickUp = (val: string) => this.setState({ orgn: val })
+    dropOff = (val: string) => this.setState({ dest: val })
 
     handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        
+
         e.preventDefault()
         // setAuth(localStorage.getItem('token'))
         let res = request('/findride', 'POST', { data: 'data sent is the following' })
@@ -103,7 +85,7 @@ interface IFormState {
     dest: string
 }
 interface IFormProps {
-    ar: any
+
 }
 
 export default Form;
