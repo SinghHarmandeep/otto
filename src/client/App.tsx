132e8be2f45f412ad46a86f52032328221c381a7
Harmandeep Singh <deepharman2000@gmail.com>
2021-05-27 21:16:23 -0400

    changed the bootstrap import location

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 
 import Routers from './routers';
 
-// import './scss/app.scss'; //use this or the next line to use bootstrap
-import '../../node_modules/bootstrap/scss/bootstrap.scss';
+import './scss/app.scss'; //use this or the next line to use bootstrap
+// import '../../node_modules/bootstrap/scss/bootstrap.scss';
 
 const dd = () => {
     
