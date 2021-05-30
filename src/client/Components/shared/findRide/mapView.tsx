0aa4ea15a94d341950fc56f77be0b727af00d553
Harmandeep Singh <deepharman2000@gmail.com>
2021-05-29 22:31:26 -0400

    accepting gcp api dynamically based on the enviroment

--- src/client/Components/shared/findRide/mapView.tsx
+++ src/client/Components/shared/findRide/mapView.tsx
@@ -4,13 +4,13 @@ import { request } from '../../../utils/app';
 
 const mapView = (props: Iprops) => {
 
+    let key: string;
 
     let res = request('../getapi', 'GET');
-    res.then(res => console.log(res)
-    ).catch(err => console.log('found an errorrrr')
+    res.then(res => key = res.data
+    ).catch(err => console.log(err)
     )
 
-    let key = null;
 
     let zoom: number = 7
     let url: string = `https://www.google.com/maps/embed/v1/directions?key=${key}&origin=${props.orig}
