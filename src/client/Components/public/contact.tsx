ef9c499d2c6dc7eab8770c081bd0f67900a0335b
Harmandeep Singh <deepharman2000@gmail.com>
2021-05-30 21:06:36 -0400

    contact page conected to server

--- src/client/Components/public/contact.tsx
+++ src/client/Components/public/contact.tsx
@@ -1,50 +1,61 @@
-import React, { FormEvent } from 'react';
+import React, { FormEvent, useState } from 'react';
 import { request } from "../../utils/app";
 
-// let email : string, phone : string, msg : string
 const contact = () => {
-    return (
-        <div className="container text-center">
-            <h1>Contact us</h1>
-            <form onSubmit={e => handleSubmit(e)} >
-                <div className="row justify-content-center form-group">
-                    <input className="col-sm-4 form-control"
-                        type="email"
-                        name="email"
-                        // value={email}
-                        placeholder='Email' />
-                </div>
-                <div className="row justify-content-center form-group">
-                    <input className="col-sm-4 form-control"
-                        type="string"
-                        name="number"
-                        // value={phone}
-                        placeholder='123-123-1234' />
-                </div>
-                <div className="row justify-content-center form-group">
-                    <textarea className="col-sm-4 form-control"
-                        rows={5}
-                        name="inFile"
-                        // value={msg} 
-                        placeholder="message" />
-                </div>
-                <div className="row justify-content-center form-group">
-                    <button className="col-sm-4 btn-primary form-control">
-                        Send
+    const [email, setEmail] = useState('')
+    const [phone, setPhone] = useState('')
+    const [msg, setMsg] = useState('')
+
+    let contactForm = < div className="container text-center" >
+        <h1>Contact us</h1>
+        <form onSubmit={e => handleSubmit(e)} >
+            <div className="row justify-content-center form-group">
+                <input className="col-sm-4 form-control"
+                    type="email"
+                    name="email"
+                    value={email}
+                    onChange={e => { setEmail(e.target.value) }}
+                    placeholder='Email' />
+            </div>
+            <div className="row justify-content-center form-group">
+                <input className="col-sm-4 form-control"
+                    type="string"
+                    name="number"
+                    value={phone}
+                    onChange={e => { setPhone(e.target.value) }}
+                    placeholder='123-123-1234' />
+            </div>
+            <div className="row justify-content-center form-group">
+                <textarea className="col-sm-4 form-control"
+                    rows={5}
+                    name="inFile"
+                    value={msg}
+                    onChange={e => { setMsg(e.target.value) }}
+                    placeholder="message" />
+            </div>
+            <div className="row justify-content-center form-group">
+                <button className="col-sm-4 btn-primary form-control">
+                    Send
                 </button>
-                </div>
-            </form >
-        </div >
+            </div>
+        </form >
+    </div >
+    return (
+        contactForm
     )
+    function handleSubmit(e: FormEvent<HTMLFormElement>) {
+        let res = request('/contact', 'POST',
+            { email, phone, msg }
+        )
+        res.then(res => {
+                contactForm = <p>message recieved</p>
+        }).catch(err => {
+                contactForm = <p>error occured in the server</p>
+        })
+        // e.preventDefault()
+    }
 }
 
-function handleSubmit(e: FormEvent<HTMLFormElement>) {
-    request('/contact', 'POST'
-        // { email, phone, msg }
-    )
-    e.preventDefault()
-
-}
 
 
 export default contact;
--- src/server/routes/api/contact.ts
+++ src/server/routes/api/contact.ts
@@ -7,7 +7,6 @@ router.post('/contact',
     (req, res) => {
         //data from request
         let recipt = req.body;
-        console.log(recipt);
         
         res.status(202).json({msg: 'contact sent',recipt});
 
