2533e2a19a08a5ee03ff7e65f25a63f578158272
Harmandeep Singh <deepharman2000@gmail.com>
2021-06-06 20:05:33 -0400

    added *

--- src/client/Components/shared/findRide/detailsForm.tsx
+++ src/client/Components/shared/findRide/detailsForm.tsx
@@ -12,31 +12,31 @@ class Details extends React.Component<IDetailsProps> {
                 onSubmit={e => this.props.submit(e)}
                 className='my-2'>
 
-                <label className="mt-3">Pick up address:</label>
+                <label className="mt-3"> <span className='text-danger'>*</span>Pick up address:</label>
                 <input type="text" className="form-control"
                     value={this.props.orgn} name="pickup" required={true}
                     onChange={e => { this.props.setPickUp(e.target.value) }} placeholder="123 pickup street" />
 
-                <label className="mt-3">Item Discription:</label>
+                <label className="mt-3"> <span className='text-danger'>*</span>Item Discription:</label>
                 <input type="text" className="form-control"
                     value={this.props.item} name="item" required={true}
                     onChange={e => this.props.setItem(e.target.value)} placeholder="Book, Keys, Food, package etc" />
 
 
-                <label className="mt-3">Drop off address:</label>
+                <label className="mt-3"> <span className='text-danger'>*</span>Drop off address:</label>
                 <input type="text" className="form-control"
                     value={this.props.dest} name="dropoff" required={true}
                     onChange={e => { this.props.setDropOff(e.target.value) }} placeholder="123 drop street" />
 
                 <div className="row mt-3">
                     <div className='col'>
-                        <label>Date:</label>
+                        <label> <span className='text-danger'>*</span>Date:</label>
                         <input className="form-control col" type="date" name="pickupdate" required={true} />
                     </div>
 
                     <div className='col'>
-                        <label>Time:</label>
-                        <input className="form-control col" type="time" name="pickuptime"  required={true} />
+                        <label><span className='text-danger'>*</span>Time:</label>
+                        <input className="form-control col" type="time" name="pickuptime" required={true} />
                     </div>
                 </div>
                 <button type="submit" className="btn btn-primary col my-3">Let's go!</button>
