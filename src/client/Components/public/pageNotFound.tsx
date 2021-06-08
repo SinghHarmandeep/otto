5d77060c0248888d754c84b53f5c0212e1bc68d7
Harmandeep Singh <deepharman2000@gmail.com>
2021-06-07 23:41:47 -0400

    finished page not found error page

--- src/client/Components/public/pageNotFound.tsx
+++ src/client/Components/public/pageNotFound.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 import { FaRegFrownOpen } from 'react-icons/fa';
@@ -11,14 +11,19 @@ const error = () => {
                 <title>404 page not found</title>
             </Helmet>
             <div className="d-flex justify-content-center">
-                <FaRegFrownOpen style={{ width: '200px', height: '200px' }} />
-                <BsCloud style={{ width: '200px', height: '200px' }} />
-                <h1 className="d-flex justify-content-center text-white">You seem lost!</h1>
+
+                <FaRegFrownOpen style={{ width: '400px', height: '400px' }} />
+
+                <div className='d-flex rounded-circle border' style={{ height: '40px', width: '40px', marginTop: '75px' }} />
+                <h1 className="d-flex justify-content-center text-white mt-3 rounded-pill border px-2"
+                    style={{ height: 'fit-content' }}>
+                    You seem lost!
+                </h1>
             </div>
             <Link className='translate-middle d-flex justify-content-center btn btn-primary btn-lg btn-block' to='/'>
-                Let's go back home!
+                Let's get you home!
                 </Link>
-        </div>
+        </div >
     )
 }
 
