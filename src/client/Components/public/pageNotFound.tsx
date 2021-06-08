f8be17f97a33ca7582ace2340df3fd65f7f048df
Harmandeep Singh <deepharman2000@gmail.com>
2021-06-07 22:58:23 -0400

    introduced react-icons

--- src/client/Components/public/pageNotFound.tsx
+++ src/client/Components/public/pageNotFound.tsx
@@ -1,16 +1,24 @@
 import React, { Fragment } from 'react';
 import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
+import { FaRegFrownOpen } from 'react-icons/fa';
+import { BsCloud } from 'react-icons/bs';
 
 const error = () => {
     return (
-            <div>
-                <Helmet>
-                    <title>404 page not found</title>
-                </Helmet>
-                <h1 className="d-flex justify-content-center text-danger">404 page not found</h1>
-                <Link className='d-flex justify-content-center rounded border border-light' to='/'>Return Home</Link>
+        <div className='container'>
+            <Helmet>
+                <title>404 page not found</title>
+            </Helmet>
+            <div className="d-flex justify-content-center">
+                <FaRegFrownOpen style={{ width: '200px', height: '200px' }} />
+                <BsCloud style={{ width: '200px', height: '200px' }} />
+                <h1 className="d-flex justify-content-center text-white">You seem lost!</h1>
             </div>
+            <Link className='translate-middle d-flex justify-content-center btn btn-primary btn-lg btn-block' to='/'>
+                Let's go back home!
+                </Link>
+        </div>
     )
 }
 
